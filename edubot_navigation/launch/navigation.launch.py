59ff58528be89b3af34aa0dd21fa3159b79e00f2
Akhsff <afkhasanyanoff@yandex.ru>
2025-12-18 22:01:22 +0300

    add nav2_bt_navigator

--- edubot_navigation/launch/navigation.launch.py
+++ edubot_navigation/launch/navigation.launch.py
@@ -7,6 +7,7 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Comm
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
 
     pkg_edubot = get_package_share_directory('edubot_navigation')
